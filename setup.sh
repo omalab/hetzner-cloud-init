47feb8b03435be56eab95d4dc5bfc5bd66a5ab6f
William Flanagan <wflanagan@audienti.com>
2021-05-20 19:33:16 -0400

    update

--- setup.sh
+++ setup.sh
@@ -37,12 +37,11 @@ wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x jq-linux64
 mv jq-linux64 /usr/local/bin/jq
 
-
-curl -o /usr/local/bin/update-config.sh https://raw.githubusercontent.com/vitobotta/hetzner-cloud-init/master/update-config.sh
+curl -o /usr/local/bin/update-config.sh https://raw.githubusercontent.com/omalab/hetzner-cloud-init/master/update-config.sh
 
 chmod +x /usr/local/bin/update-config.sh
 
-ufw allow proto tcp from any to any port 22,80,443
+ufw allow proto tcp from any to any port 22,80,443,6443,8080,10250,10251,10252
 
 IFS=', ' read -r -a WHITELIST <<< "$WHITELIST_S"
 
@@ -50,10 +49,9 @@ for IP in "${WHITELIST[@]}"; do
   ufw allow from "$IP"
 done
 
-ufw allow from 10.43.0.0/16
-ufw allow from 10.42.0.0/16
-ufw allow from 10.0.0.0/16 # default private network cidr
-ufw allow from 10.244.0.0/16 # in case we use the default cidr expected by the cloud controller manager
+ufw allow from 10.0.0.0/24
+ufw allow from 10.0.1.0/24
+ufw allow from 10.244.0.0/24 # in case we use the default cidr expected by the cloud controller manager
 
 ufw -f default deny incoming
 ufw -f default allow outgoing

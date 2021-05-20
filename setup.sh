b514e1ed1e8a4377eb3412478c95618239707d96
William Flanagan <wflanagan@audienti.com>
2021-05-20 19:36:35 -0400

    Udpate

--- setup.sh
+++ setup.sh
@@ -49,8 +49,9 @@ for IP in "${WHITELIST[@]}"; do
   ufw allow from "$IP"
 done
 
-ufw allow from 10.0.0.0/24
+ufw allow from 10.0.0.0/16
 ufw allow from 10.0.1.0/24
+ufw allow from 10.0.0.0/24
 ufw allow from 10.244.0.0/24 # in case we use the default cidr expected by the cloud controller manager
 
 ufw -f default deny incoming
